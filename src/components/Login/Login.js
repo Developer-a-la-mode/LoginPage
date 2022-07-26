904d5e3e683237c7681f196a8bb42d10ef03f68e
Kevin Tonthat <tonthat.kevin@gmail.com>
2022-07-25 23:16:52 -0700

    cleartimeout function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,12 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
+    const identifier = setTimeout(() => {
       console.log('checking form for validity');
       setFormIsValid(
         enteredEmail.includes('@') && enteredPassword.trim().length > 6
       );
     }, 2000);
+
+    return () => {
+      console.log('Clean Up');
+      clearTimeout(identifier);
+    };}, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
